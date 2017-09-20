4d74d0e180fc13ae2428e428b74c9dc4b81a5100
BJZE <BJZE@MI00319366.petrobras.biz>
2017-09-20 07:10:12 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  private logoSansim = require("./../assets/img/logo_sansim.jpg");
+//  private logo = require("./../assets/img/logo.jpg");
 }
