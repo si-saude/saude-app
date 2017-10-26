6238a00fcf384164e9ee233348036db65a414459
bk6i <bk6i@MI00313395.petrobras.biz>
2017-10-26 13:42:57 -0300

    Profissiograma

--- src/app/controller/profissiograma/profissiograma-form/profissiograma-form.component.ts
+++ src/app/controller/profissiograma/profissiograma-form/profissiograma-form.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import {MaterializeDirective,MaterializeAction} from "angular2-materialize";
 
 import { GlobalVariable } from './../../../global'; 
 import { Profissiograma } from './../../../model/profissiograma';
 import { ProfissiogramaService } from './../profissiograma.service';
 import { ProfissiogramaFilter } from './../profissiograma.filter';
 import { GrupoMonitoramento } from './../../../model/grupo-monitoramento';
+import { GrupoMonitoramentoExame } from './../../../model/grupo-monitoramento-exame';
+import { GrupoMonitoramentoExameBuilder } from './../../grupo-monitoramento-exame/grupo-monitoramento-exame.builder';
+import { Exame } from './../../../model/exame';
+import { ExameBuilder } from './../../exame/exame.builder';
+import { Criterio } from './../../../model/criterio';
+import { CriterioBuilder } from './../../criterio/criterio.builder';
 import { GenericFormComponent } from './../../../generics/generic.form.component'; 
 import { ProfissiogramaBuilder } from './../profissiograma.builder';
 import { GrupoMonitoramentoBuilder } from './../../grupo-monitoramento/grupo-monitoramento.builder';
@@ -18,8 +26,16 @@ import { GrupoMonitoramentoBuilder } from './../../grupo-monitoramento/grupo-mon
 export class ProfissiogramaFormComponent extends GenericFormComponent<Profissiograma> {
     profissiograma: Profissiograma;
     gruposMonitoramento: Array<GrupoMonitoramento>;
-//    dtOptions: DataTables.Settings = {};
-//    dtTrigger: Subject = new Subject();
+    gruposMonitoramentoExame: Array<GrupoMonitoramentoExame>;
+    exames: Array<Exame>;
+    criterios: Array<Criterio>;
+    arrayCriterio: Array<Criterio>;
+    
+    globalActions = new EventEmitter<string|MaterializeAction>();
+    toastParams = ['', 4000];
+
+    selectedGM = null;
+    selectedExm = null;
 
     profissiogramaFilter: ProfissiogramaFilter = new ProfissiogramaFilter();
     
@@ -29,14 +45,12 @@ export class ProfissiogramaFormComponent extends GenericFormComponent<Profissiog
         this.goTo = "profissiograma";
         
         this.profissiograma = new ProfissiogramaBuilder().initialize(this.profissiograma);
+        
+        this.gruposMonitoramentoExame = new Array<GrupoMonitoramentoExame>();
+        this.arrayCriterio = new Array<Criterio>();
     }
 
     ngOnInit() {
-//        this.dtOptions = {
-//            paging: false,
-//            seaching: false,
-//            displayLength: 2
-//        };
         this.inscricao = this.route.params.subscribe(
             ( params: any ) => {
                 if( params['id'] !== undefined ) {
@@ -62,23 +76,92 @@ export class ProfissiogramaFormComponent extends GenericFormComponent<Profissiog
               .catch(error => {
                   console.log(error);
               })
+          
+          this.profissiogramaService.getExames()
+              .then(res => {
+                  this.exames = res.json();
+              })
+              .catch(error => {
+                  console.log(error);
+              })
+          
+          this.profissiogramaService.getCriterios()
+          .then(res => {
+              this.criterios = res.json();
+          })
+          .catch(error => {
+              console.log(error);
+          })
       }
         
     save() {
         super.save(new ProfissiogramaBuilder().clone(this.profissiograma));
     }
-  
-    addExame() {
+    
+    addGrupoMonitoramento(valor: number) {
+        let grupoMonitoramento = this.gruposMonitoramento.find(o => o["id"] == valor);
+        this.profissiograma.getGrupoMonitoramentos().push(new GrupoMonitoramentoBuilder().clone(grupoMonitoramento));
     }
     
 
-    removeExame(i: number) {
+    removeGrupoMonitoramento(i: number) {
+        this.profissiograma.getGrupoMonitoramentos().splice(i, 1);
     }
     
-    addCriterio() {
+    selectGrupoMonitoramento(index: number) {
+        this.selectedGM = this.gruposMonitoramento[index];
+        this.gruposMonitoramentoExame = this.profissiograma.getGrupoMonitoramentos()[index].getGrupoMonitoramentoExames();
+        this.arrayCriterio = new Array<Criterio>();
+    }
+  
+    addExame(valor: number) {
+        if ( this.selectedGM === null ) { 
+            this.toastParams = ['Por favor, escolha um grupo monitoramento', 4000];
+            this.globalActions.emit('toast');
+        } else {
+            let exame = this.exames.find(o => o["id"] == valor);
+            let grupoMonitoramentoExame = new GrupoMonitoramentoExameBuilder().initialize(new GrupoMonitoramentoExame());
+            grupoMonitoramentoExame.setExame(new ExameBuilder().clone(exame));
+            grupoMonitoramentoExame.setCriterios(new CriterioBuilder().initializeList(new Array<Criterio>()));
+            
+            this.gruposMonitoramentoExame.push(grupoMonitoramentoExame);
+        }
+    }
+
+    removeExame(i: number) {
+        this.gruposMonitoramentoExame.splice(i, 1);
+    }
+
+    selectExame(index: number) {
+        this.selectedExm = this.gruposMonitoramentoExame[index].getExame();
+        this.arrayCriterio = this.gruposMonitoramentoExame[index].getCriterios();
+    }
+    
+    addCriterio(valor: number) {
+        if ( this.selectedExm === null ) { 
+            this.toastParams = ['Por favor, escolha um exame', 4000];
+            this.globalActions.emit('toast');
+        } else {
+            let criterio = this.criterios.find(o => o["id"] == valor);
+            this.arrayCriterio.push(criterio);
+            console.log(this.arrayCriterio);
+        }
     }
 
     removeCriterio(i: number) {
+        this.arrayCriterio.splice(i, 1);
+    }
+    
+    selectedGrupoMonitoramento(gM: number) {
+        if ( this.gruposMonitoramento[gM] === this.selectedGM ) {
+            return "active";
+        } else return "";
+    }
+    
+    selectedExame(e: number) {
+        if ( this.gruposMonitoramentoExame[e].getExame() === this.selectedExm ) {
+            return "active";
+        } else return "";
     }
     
     isPossibleDeactivate() {
