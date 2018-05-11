6c535dc506e08069a381ec3b5a585da60838812f
bk6i <bk6i@MI00313395.petrobras.biz>
2018-05-11 14:17:38 -0300

    .

---
+++ src/app/generics/date.util.ts
@@ -0,0 +1,38 @@
+export class DateUtil {
+    
+    protected parseDataToObjectDatePicker( data ) {
+        if ( data === undefined || data === null ) {
+            return undefined;
+        }
+        let s = data.split( "T" );
+        let datas = s[0].split( "-" );
+        if ( datas[2].substring( 0, 1 ) === "0" ) {
+            datas[2] = datas[2].replace( "0", "" );
+        }
+        if ( datas[1].substring( 0, 1 ) === "0" ) {
+            datas[1] = datas[1].replace( "0", "" );
+        }
+        let o = Object.create( { date: { year: datas[0], month: datas[1], day: datas[2] } } );
+        return o;
+    }
+
+    protected parseDatePickerToDate( data ) {
+        if ( data === undefined || data === null ) {
+            return null;
+        } else if ( data instanceof Date ) {
+            return data;
+        }
+        let d: Date = new Date( data.date.year, data.date.month - 1, data.date.day );
+        return d;
+    }
+
+    protected parseDataToString( data ) {
+        if ( data === undefined || data === null ) {
+            return undefined;
+        }
+        let s = data.split( "T" );
+        let datas = s[0].split( "-" );
+
+        return datas[2] + "/" + datas[1] + "/" + datas[0];
+    }
+}
--- src/app/generics/generic.component.ts
+++ src/app/generics/generic.component.ts
@@ -6,6 +6,8 @@ import { MaterializeAction } from "angular2-materialize";
 
 import { HTMLStatus } from './../html-status';
 
+import { DateUtil } from 'date.util.ts';
+
 export abstract class GenericComponent {
     protected showPreload: boolean;
     protected msgPreload: string;
@@ -61,42 +63,6 @@ export abstract class GenericComponent {
                 break;
         }
 
-    }
-    
-    parseDataToObjectDatePicker( data ) {
-        if ( data === undefined || data === null ) {
-            return undefined;
-        }
-        let s = data.split( "T" );
-        let datas = s[0].split( "-" );
-        if ( datas[2].substring( 0, 1 ) === "0" ) {
-            datas[2] = datas[2].replace( "0", "" );
-        }
-        if ( datas[1].substring( 0, 1 ) === "0" ) {
-            datas[1] = datas[1].replace( "0", "" );
-        }
-        let o = Object.create( { date: { year: datas[0], month: datas[1], day: datas[2] } } );
-        return o;
-    }
-
-    parseDatePickerToDate( data ) {
-        if ( data === undefined || data === null ) {
-            return null;
-        } else if ( data instanceof Date ) {
-            return data;
-        }
-        let d: Date = new Date( data.date.year, data.date.month - 1, data.date.day );
-        return d;
-    }
-
-    parseDataToString( data ) {
-        if ( data === undefined || data === null ) {
-            return undefined;
-        }
-        let s = data.split( "T" );
-        let datas = s[0].split( "-" );
-
-        return datas[2] + "/" + datas[1] + "/" + datas[0];
     }
 
 }
