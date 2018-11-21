14a948003c4421218c77210583bfdd0b7de8b86e
bw8w <bw8w@MI00312805.petrobras.biz>
2018-11-21 09:23:26 -0300

    .

--- src/app/controller/empregado-convocacao/empregado-convocacao.component.ts
+++ src/app/controller/empregado-convocacao/empregado-convocacao.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { MaterializeAction } from "angular2-materialize";
 
 import { GlobalVariable } from './../../global';
 import { EmpregadoConvocacao } from './../../model/empregado-convocacao';
@@ -16,10 +18,31 @@ import { GenericListComponent } from './../../generics/generic.list.component';
 
 export class EmpregadoConvocacaoComponent 
     extends GenericListComponent<EmpregadoConvocacao, EmpregadoConvocacaoFilter, EmpregadoConvocacaoGuard> {
+    @ViewChild( 'arquivoTxt' ) inputElArquivoTxt: ElementRef;
     
     constructor(empregadoConvocacaoService: EmpregadoConvocacaoService,
                 empregadoConvocacaoGuard: EmpregadoConvocacaoGuard,
                 router: Router) {
         super(empregadoConvocacaoService, new EmpregadoConvocacaoFilter(), empregadoConvocacaoGuard, router);
     }   
+    
+    
+    importFileTxt() {
+        let arquivo = undefined;
+        
+        if(this.inputElArquivoTxt.nativeElement.files.length > 0){
+            arquivo = this.inputElArquivoTxt.nativeElement.files[0];
+            this.showPreload = true;
+            this.service.sendFileWithPath(arquivo, "import-txt")
+                .then(res => {
+                    this.showPreload = false;
+                    location.reload();
+                })
+                .catch(error => {
+                    this.catchConfiguration(error);
+                })
+        } else {
+            console.log('falta selecionar arquivo');
+        }
+    }
 }
